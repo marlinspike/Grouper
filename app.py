312f9bef08cf7ca37f0975df6aac02cc6388b95a
Reuben Cleetus <rcleetus@gmail.com>
2019-05-07 14:24:25 -0400

    Prefereces implemented. Auto-gen sample file from Prefs

--- app.py
+++ app.py
@@ -3,19 +3,37 @@ from network.securitygroup.securityrule import SecurityRule
 from parser.csvparser import CSVParser
 from network.securitygroup.nsglist import NSGList
 from parser.armwriter import ARMWriter
+import os
+import sys
+from prefs.prefs import Preferences
 
 #from network.networksecuritygroups.securityrule import SecurityRule
 #import network.networksecuritygroups.securityrule.SecurityRule
 
 @click.command()
-@click.option('--csv', default="grouper-nsg.csv", help='Network Security Rules CSV file to use')
-@click.option('--prefs', default=None, help='Grouper preferences json file to use')
-@click.option('--genpref', default=None, help='True/False. Generate sample preferences json file')
-def doIt(csv, prefs, genpref):
+@click.option('--datafile', default="grouper-sample.csv", help='CSV data file to use')
+@click.option('--genfile/--nogen', default=False, help='True/False. Generate sample preferences json file')
+def doIt(datafile, genfile):
 
 
     print("--------")
-    parser = CSVParser("/Users/reuben/code/python/Grouper/grouper-sample.csv")
+    prefs = Preferences()
+    os_path = os.path.abspath(os.path.dirname(__file__))
+    
+    if(genfile == True):
+        prefs.generateSampleDataFile(os_path)
+        print(f"Sample data file generated! You can customize it, or generate templates based on the sample data.")
+        sys.exit(200)
+
+    
+    
+    grouper_file = os.path.join(os_path, datafile)
+    file_exists = os.path.isfile(grouper_file)
+    if(file_exists == False):
+            print(f"Error: There was an issue reading in the data file {datafile}.\n\nPlease use the --genfile option to generate a sample data file to customize.")
+            sys.exit(200)
+
+    parser = CSVParser(grouper_file) #"/Users/reuben/code/python/Grouper/grouper-sample.csv")
     nsglist = parser.doCSVParse()
     print(nsglist.nsg_rg_lookup)
 
--- prefs/prefs.py
+++ prefs/prefs.py
@@ -1,5 +1,54 @@
+import sys
+import json
+import os
+from datetime import datetime
+
 class Preferences:
 
     def __init__(self):
-        self.lst_ignore_priority = [65000, 65001, 65500] #Reserved numbers in Azure. Do not reuse
+        self.lst_ignore_priority = []; [65000, 65001, 65500] #Reserved numbers in Azure. Do not reuse
+        self.lst_ignore_priority = self.load_prefs('ignore_priority').split(",")
+        self.sample_file = ""
+    
+    ###
+    # Loads preferences
+    def load_prefs(self, prefName) -> str:
+        os_path = os.path.abspath(os.path.dirname(__file__))
+        prefsFile = os.path.join(os_path, "prefs.json")
+        preference = ""
+
+        #prefsFile = './prefs.json'
+        today = datetime.now().strftime('%Y-%m-%d')
+        try:
+            with open(prefsFile) as cred_data:
+                prefs = json.load(cred_data)
+                preference = prefs[prefName]
+        except Exception as e:
+            msg = 'There was an issue reading in the credentials file ' + prefsFile
+            print(msg)
+            print('[ERROR] ' + str(e))
+            sys.exit(200)
+
+        return preference
+
+
+    ###
+    # Generates a sample data file to use
+    def generateSampleDataFile(self, path):
+        grouper_file = os.path.join(path, "grouper-sample.csv")
+        file_exists = os.path.isfile(grouper_file)
+        overwrite_file:str = "n"
+        sample_data_file = self.load_prefs('sample_data_file')
+
+
+        if(file_exists == True):
+            overwrite_file = input("Sample data file already exists. Overwrite? (y/n) or ENTER to cancel > ")
+            if(overwrite_file.lower().startswith("n")):
+                return
         
+        #Continue to create file
+        text_file = open(grouper_file, "w", newline='')
+        for line in sample_data_file:
+            text_file.write(line + '\n')
+        text_file.close()
+
