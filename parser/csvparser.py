28b80e5ea61274af7e52f5afb365abfb7f7d056a
Reuben Cleetus <rcleetus@gmail.com>
2019-05-08 14:32:05 -0400

    Handle rule priority ignore list

--- parser/csvparser.py
+++ parser/csvparser.py
@@ -1,11 +1,13 @@
 import csv
 from network.securitygroup.securityrule import SecurityRule
 from network.securitygroup.nsglist import NSGList
+from prefs.prefs import Preferences
 
 class CSVParser:
 
     def __init__(self, filename):
         self.filename = filename
+        self.prefs = Preferences()
     
     ###
     # Parse the CSV File to populate an NSGList with a SecurityRule object for each row
@@ -14,9 +16,12 @@ class CSVParser:
         nsglist = NSGList()
         with open(self.filename) as csvFile:
             reader = csv.DictReader(csvFile)
+            
             for row in reader:
-                rule = SecurityRule().createFromOrderedDict(row)
-                nsglist.addToDict(rule)
-                nsglist.nsg_rg_lookup[row["nsgname"]] = row["resource_group"]
+                #Only add a rule if its priority is not in the Ignore List
+                if (not row["priority"] in self.prefs.lst_ignore_priority):
+                    rule = SecurityRule().createFromOrderedDict(row)
+                    nsglist.addToDict(rule)
+                    nsglist.nsg_rg_lookup[row["nsgname"]] = row["resource_group"]
 
         return nsglist
